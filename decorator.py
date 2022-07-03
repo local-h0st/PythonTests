c06edd4e6ad19777b5258e95574a81ab3cb58cb1
local-h0st <2485885613@qq.com>
2022-07-03 18:50:30 +0800

    Edit decorator.py

--- decorator.py
+++ decorator.py
@@ -25,21 +25,22 @@ print(rst)
 
 print(func2(2))
 
-
 ###################################
+print('########## Basic usage ############')
+
 
 def decoratorFunction(name_of_func_to_be_decorated):
-    def decoratedFunc(*args, **kwargs):
+    def decoratedFunc(*args, **kwargs):  # (*args, **kw)使decoratedFunc()函数可以接受任意参数的调用。
         # decorating...
         print('[Decorator]')
         # decorator ended
-        return name_of_func_to_be_decorated(*args, **kwargs)    # return的时候会调用原来的函数！是调用！
+        return name_of_func_to_be_decorated(*args, **kwargs)  # return的时候会调用原来的函数！是调用！
 
     return decoratedFunc
 
 
 # Way 1:
-@decoratorFunction
+@decoratorFunction  # decoratorFunction()返回装饰后的函数，只要写decoratorFunction不用写()
 def originFunc(a):
     print('hello', a)
     return a
@@ -59,6 +60,72 @@ f = decoratorFunction(originFunc2)
 print(originFunc2(666))
 print(f(666))
 
-#################################
+####################################################
+
+# Advanced
+
+print('####### Advanced:Free to set msgs ######')
 
 
+def oriFunc(message):
+    print('msg{' + message + '} printed from oriFunc()')
+    return 'msg{' + message + '} returned by oriFunc()'
+
+
+def h(msg):
+    def g(oriF):
+        def f(*args, **kwargs):
+            print(msg)
+            return oriF(*args, **kwargs)
+
+        return f
+
+    return g
+
+
+a = h('msg from h')
+b = a(oriFunc)
+c = b('purelove')
+print('#c', c)
+
+d = h('msg from h')(oriFunc)
+e = d('PureLov3')
+print(e)
+
+i = h('msg from h')(oriFunc)('purelov3')
+print(i)
+
+print("######## using @ ########")
+
+
+@h('[Decorator] msg from h using @')  # h()返回的不是装饰后的函数，而是一个能够生成返回装饰后函数的函数，而h('xxxxx')()才返回装饰后的函数，因此不写最后的(),只写h('xxxxx')
+def ff(m):
+    print('{', m, '} printed in function ff')
+    # return '{', m, '} returned by function ff'    这么写返回的是一个tuple，而不是拼凑的字符串
+    return '{ ' + m + ' } returned by function ff'
+
+
+print(ff('msg from inside ff()'))
+
+####################################################
+
+print('########## about __name__ ##########')
+
+
+def createDecoratedFuncs(oriFunc):
+    def decoratedF(*args, **kwargs):
+        print('[ Decorator ] decorated head')
+        return oriFunc(*args, **kwargs)
+
+    return decoratedF
+
+
+@createDecoratedFuncs
+def fff(str):
+    print('[ msg printed in fff ]', str)
+    return '[ msg returned by fff ] ' + str
+
+
+rst = fff('purelove')
+print(rst)
+print('{ fff.__name__ : }',fff.__name__)
